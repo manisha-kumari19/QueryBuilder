e61dc9e95e00d599e4c182aa24b36c2c7c46018e
manisha-kumari19 <mkumari@argusoft.com>
2024-01-15 17:25:44 +0530

    recursive approach applied

---
+++ backend/src/main/java/com/QueryBuilder/dto/MethodVariableDTO.java
@@ -0,0 +1,12 @@
+package com.QueryBuilder.dto;
+
+import java.util.UUID;
+
+public class MethodVariable {
+
+    private UUID id;
+    private String variableName;
+    private String type;
+    private UUID schemaDefId;
+    private UUID methodId;
+}
